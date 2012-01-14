dbadd2c8ce7eae9aef540f501c7d847758bada4e
traker <guihej@gmail.com>
2012-01-14 10:38:21 +0100

    ajout de delai

--- lcd.cpp
+++ lcd.cpp
@@ -7,50 +7,69 @@
 #include "lcd.h"
 #include <WProgram.h>
 Lcd::Lcd(){
-  Serial1.begin(19200);
+ // Serial1.begin(19200);
 }
 
 void Lcd::ELCD_initialize(){
   Serial1.print(0xA0, BYTE);
+  delay(1);
 }
 
 void Lcd::ELCD_Cursor_OFF(){
   Serial1.print(0xA3, BYTE);
+  delay(1);
   Serial1.print(0x0C, BYTE);
+  delay(1);
 }
 
 void Lcd::ELCD_Cursor_ON(){
   Serial1.print(0xA3, BYTE);
+  delay(1);
   Serial1.print(0x0E, BYTE);
+  delay(1);
 }
 
 void Lcd::ELCD_Clear_LCD(){
   Serial1.print(0xA3, BYTE);
+  delay(1);
   Serial1.print(0x01, BYTE);
+  delay(1);
 }
 
 void Lcd::ELCD_Cursor_Position(int x, int y){
   Serial1.print(0xA1, BYTE);
+  delay(1);
   Serial1.print(x, BYTE);
+  delay(1);
   Serial1.print(y, BYTE);
+  delay(1);
 }
 
 void Lcd::ELCD_put_str(char *str){
   Serial1.print(0xA2, BYTE);
+  delay(1);
   while(*str)
     Serial1.print(*str++);
+    delay(1);
   Serial1.print(0, BYTE);
+  delay(1);
 }
 
 void Lcd::ELCD_put_ch(char ch){
   Serial1.print(0xA2, BYTE);
+  delay(1);
   Serial1.print(ch);
+  delay(1);
   Serial1.print(0, BYTE);
+  delay(1);
 }
 
 void Lcd::chargement(){
-  this->ELCD_Cursor_Position(1, 0);
-  this->ELCD_put_str(" _  _   _/   ._  _");
-  this->ELCD_put_str("/ //_ /_//_/// //_/");
+  this->ELCD_Clear_LCD();
+  this->ELCD_Cursor_Position(0, 0);
+  this->ELCD_put_str("  _  _   _/   . _  _ ");
+  this->ELCD_Cursor_Position(0, 1);
+  this->ELCD_put_str("/ //_  /_//_/// //_/ ");
+  this->ELCD_Cursor_Position(0, 2);
   this->ELCD_put_str("         by traker");
 }
